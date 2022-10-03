0206c1fc2f99008681a89ee06e301d9ef48b38c4
bubawolf <ibotero@bamboostudio.com.co>
2022-10-03 18:02:58 -0500

    initial commit 2

--- src/js/controller.js
+++ src/js/controller.js
@@ -11,6 +11,7 @@ import 'core-js/stable';
 import 'regenerator-runtime';
 import Fraction from 'fractional';
 
+console.log('Welcome Ivancho');
 ///////////////////////////////////////
 /*
 if (module.hot) {
